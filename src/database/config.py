3f7a2951765ee13ab9eef9d40bd4adc4bef02ec3
bálint szente <balint.szente@stud.uni-corvinus.hu>
2026-03-26 18:01:22 +0000

    changing dotenv import formats

--- src/database/config.py
+++ src/database/config.py
@@ -3,14 +3,10 @@ from urllib.parse import quote_plus
 
 from dotenv import load_dotenv
 
-
-DB_CONNECTION_STRING_ENV = "DB_CONNECTION_STRING"
-
-
 def get_database_url() -> str:
     load_dotenv()
-    connection_string = os.getenv(DB_CONNECTION_STRING_ENV)
+    connection_string = os.getenv("DB_CONNECTION_STRING")
     if not connection_string:
-        raise RuntimeError(f"Missing {DB_CONNECTION_STRING_ENV} environment variable")
+        raise RuntimeError(f"Missing connection string environment variable")
 
     return f"mssql+pyodbc:///?odbc_connect={quote_plus(connection_string)}"
